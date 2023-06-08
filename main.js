9c3931a325a49cfbc13bda5e77ee6b2283728b08
김종민 <104364725+kimjongk@users.noreply.github.com>
2023-06-08 21:25:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// // navbar 
+// const navbar = document.querySelector('#navbar');
+// const navbarHeight = navbar.getBoundingClientRect().height;
+// document.addEventListener('scroll', () => {
+//     if (window.scrollY > navbarHeight) {
+//         navbar.classList.add('navbar--dark');
+//     } else {
+//         navbar.classList.remove('navbar--dark');
+//     }
+// });
+
+// document.getElementById('main').play();
+
+let navbar = $('#navbar')
